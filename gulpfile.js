be6b887e554b88b3b7c949eb89b4284649caf6e4
hohadiwinata <hohadiwinata@connectome.co.nz>
2017-02-09 18:12:15 +1300

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function(){
    gulp.src(coffeeSources)
@@ -45,6 +47,8 @@ gulp.task ('compass', function () {
 gulp.task('watch', function(){
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
     gulp.watch('components/sass/*.scss', ['compass']);
 });
 
@@ -55,4 +59,14 @@ gulp.task('connect', function(){
     })
 });
 
-gulp.task ('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('html', function(){
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+gulp.task ('default', ['coffee', 'html','json', 'js', 'compass', 'connect', 'watch']);
